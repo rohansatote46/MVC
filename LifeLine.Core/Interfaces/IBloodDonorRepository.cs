a0bfa578d3e296d7798355f52ef4ad428d9435cb
rohansatote <rohansatote@gmail.com>
2017-12-30 16:21:41 +0530

    Add project files.

---
+++ LifeLine.Core/Interfaces/IBloodDonorRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LifeLine.Core.Interfaces
+{
+    public interface IBloodDonorRepository
+    {
+        void Add(BloodDonor b);
+        void Edit(BloodDonor b);
+        void Remove(string BloodDonorID);
+        IEnumerable<BloodDonor> GetBloodDonors();
+        BloodDonor FindById(string BloodDonorID);
+    }
+}
---
+++ LifeLine.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LifeLine.Web.Startup))]
+namespace LifeLine.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
